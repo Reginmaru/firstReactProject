e45afe7e0bf5fc1bf4851a6b44bc6b9705a08b39
Reginmaru <85396830+Reginmaru@users.noreply.github.com>
2022-04-18 13:00:31 +0100

    Delete first-react directory

--- first-react/src/heading.js
+++
@@ -1,7 +0,0 @@
-function Heading(){
-    return ( 
-        <h1 id="heading">Profile</h1>
-    )
-}
-
-export default Heading
--- first-react/src/heading.test.js
+++
@@ -1,8 +0,0 @@
-import { render, screen } from "@testing-library/react";
-import Heading from './heading';
-
-test('renders bigger heading', () =>{
-    render(<Heading />);
-    const bigHeading = screen.getByText(/Profile/i);
-    expect(bigHeading).toBeInTheDocument();
-  })
